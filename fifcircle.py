de3142f5afb3086b4f6ed92bdae782caf672523c
unknown <67711144+rosporaa@users.noreply.github.com>
2022-11-17 17:39:55 +0000

    1st commit

--- fifcircle.py
+++ fifcircle.py
@@ -1,32 +1,54 @@
 import sys
 
-majorScale = ["C", "G", "D", "A", "E", "B", "Fis", "Cis", "Gis", "Dis", "Bes", "F"]  #0
-minorScale = ["A mi", "E mi", "B mi", "Fis mi", "Cis mi", "Gis mi", "Dis mi", "Bes mi", "F mi", "C mi", "G mi", "D mi"]             #1
-dimisScale = ["B dim", "Fis dim", "Cis dim", "Gis dim", "Dis dim", "Bes dim", "F dim", "C dim", "G dim", "D dim", "A dim", "E dim"] #2
+findmajorScale = ["C", "G", "D", "A", "E", "B", "Fis", "Cis", "Gis", "Dis", "Bes", "F"]
+findminorScale = ["A mi", "E mi", "B mi", "Fis mi", "Cis mi", "Gis mi", "Dis mi", "Bes mi", "F mi", "C mi", "G mi", "D mi"]
+finddimisScale = ["B dim", "Fis dim", "Cis dim", "Gis dim", "Dis dim", "Bes dim", "F dim", "C dim", "G dim", "D dim", "A dim", "E dim"]
 
-# v ktorej o kolko
-#major
-ionian = [0,0] # v major o nula posun
-lydian = [0,1]
-myxoltdian = [0,-1]
-#minor
-aeolian = [1, 0]
-dorian = [1, 1]
-phrygian = [1, -1]
-#dimished
-locryan = [2, 0]
+scales = {"scales": [{"name":"ionian",     "index":"majorSidx", "shift": 0}, 
+                     {"name":"lydian",     "index":"majorSidx", "shift": 1},
+                     {"name":"myxolidian", "index":"majorSidx", "shift": -1}, 
+                     {"name":"aeonian",    "index":"minorSidx", "shift": 0}, 
+                     {"name":"dorian",     "index":"minorSidx", "shift": 1}, 
+                     {"name":"phrygian",   "index":"minorSidx", "shift": -1}, 
+                     {"name":"locryan",    "index":"dimisSidx", "shift": 0} 
+                    ]
+         }
 
 while True:
-  tone = input("Enter scale name (C, G, D, A, E, B, F, Fis, Cis, Gis, Dis, Bes, k-koniec): ")
+  tone = input("Enter scale name (C, G, D, A, E, B, F, Fis, Cis, Gis, Dis, Bes, k-end): ")
   if tone == 'k':
     sys.exit(0)
-  if tone in majorScale:
+  if tone in findmajorScale:
     break
 
-majorSidx = majorScale.index(tone)
-minorSidx = minorScale.index(tone + " mi")
-dimisSidx = dimisScale.index(tone + " dim")
+majorSidx = findmajorScale.index(tone)
+minorSidx = findminorScale.index(tone + " mi")
+dimisSidx = finddimisScale.index(tone + " dim")
 
-print (f"{majorScale[majorSidx]}, {minorScale[minorSidx]}, {dimisScale[dimisSidx]}")
+for i in scales["scales"]:
+  print (i["name"])
+  idx = eval(i["index"]) + i["shift"]
+  if idx > (len(findmajorScale) - 1):
+    idx = 0
+  if idx < 0:
+    idx = len(findmajorScale) - 1
 
+
+  ileft  = idx - 1
+  iright = idx + 1
+
+  if ileft > (len(findmajorScale) - 1):
+    ileft = 0
+  if ileft < 0:
+    ileft = len(findmajorScale) - 1
+
+  if iright > (len(findmajorScale) - 1):
+    iright = 0
+  if iright < 0:
+    iright = len(findmajorScale) - 1
+  
+  print (f"Major:  {findmajorScale[ileft]} - {findmajorScale[idx]} - {findmajorScale[iright]}")
+  print (f"Minor:  {findminorScale[ileft]} - {findminorScale[idx]} - {findminorScale[iright]}")
+  print (f"Dimis:      - {finddimisScale[idx]} - ")
+  
 
