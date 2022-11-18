91af60e60cab2c1803f16266512028f13a6cf92a
unknown <67711144+rosporaa@users.noreply.github.com>
2022-11-18 08:28:30 +0000

    changes

--- fifcircle.py
+++ fifcircle.py
@@ -4,18 +4,18 @@ findmajorScale = ["C", "G", "D", "A", "E", "H", "Fis", "Cis", "Gis", "Dis", "B",
 findminorScale = ["A mi", "E mi", "H mi", "Fis mi", "Cis mi", "Gis mi", "Dis mi", "B mi", "F mi", "C mi", "G mi", "D mi"]
 finddimisScale = ["H dim", "Fis dim", "Cis dim", "Gis dim", "Dis dim", "B dim", "F dim", "C dim", "G dim", "D dim", "A dim", "E dim"]
 
-scales = {"scales": [{"name":"ionian",     "index":"majorSidx", "shift": 0}, 
-                     {"name":"lydian",     "index":"majorSidx", "shift": 1},
-                     {"name":"myxolydian", "index":"majorSidx", "shift": -1}, 
-                     {"name":"aeonian",    "index":"minorSidx", "shift": 0}, 
-                     {"name":"dorian",     "index":"minorSidx", "shift": 1}, 
-                     {"name":"phrygian",   "index":"minorSidx", "shift": -1}, 
-                     {"name":"locryan",    "index":"dimisSidx", "shift": 0} 
+scales = {"scales": [{"name":"Ionian",     "index":"majorSidx", "shift": 0}, 
+                     {"name":"Lydian",     "index":"majorSidx", "shift": 1},
+                     {"name":"Myxolydian", "index":"majorSidx", "shift": -1}, 
+                     {"name":"Aeonian",    "index":"minorSidx", "shift": 0}, 
+                     {"name":"Dorian",     "index":"minorSidx", "shift": 1}, 
+                     {"name":"Phrygian",   "index":"minorSidx", "shift": -1}, 
+                     {"name":"Locryan",    "index":"dimisSidx", "shift": 0} 
                     ]
          }
 
 while True:
-  tone = input("Enter scale name (C, G, D, A, E, B, F, Fis, Cis, Gis, Dis, Bes, k-end): ")
+  tone = input("Enter scale name (" + ', '.join(findmajorScale) + ", k-end): ")
   if tone == 'k':
     sys.exit(0)
   if tone in findmajorScale:
@@ -25,8 +25,9 @@ majorSidx = findmajorScale.index(tone)
 minorSidx = findminorScale.index(tone + " mi")
 dimisSidx = finddimisScale.index(tone + " dim")
 
+print (f"\n@@@ Scale {tone} @@@")
 for i in scales["scales"]:
-  print (f"\n{i['name']}: \n ---------------")
+  print (f"\n@ {i['name']}: \n ---------------")
   idx = eval(i["index"]) + i["shift"]
   if idx > (len(findmajorScale) - 1):
     idx = 0
@@ -47,8 +48,8 @@ for i in scales["scales"]:
   if iright < 0:
     iright = len(findmajorScale) - 1
   
-  print (f"  Major:  {findmajorScale[ileft]:6} - {findmajorScale[idx]:6} - {findmajorScale[iright]:6}")
-  print (f"  Minor:  {findminorScale[ileft]:6} - {findminorScale[idx]:6} - {findminorScale[iright]:6}")
+  print (f"  Major:  {findmajorScale[ileft]:6}   {findmajorScale[idx]:6}   {findmajorScale[iright]:6}")
+  print (f"  Minor:  {findminorScale[ileft]:6}   {findminorScale[idx]:6}   {findminorScale[iright]:6}")
   print (f"  Dimis:           {finddimisScale[idx]:6}")
 
 
